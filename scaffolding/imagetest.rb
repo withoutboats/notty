4d1d1fbfb297c86f3a52e96f7f359f4b846cd7d6
Without Boats <woboats@gmail.com>
2015-11-12 16:05:50 -0800

    NoFeature instead of panic for unknown mode sets.

--- scaffolding/imagetest.rb
+++ scaffolding/imagetest.rb
@@ -1,10 +1,2 @@
-#!/usr/sbin/ruby
-
-img = IO.binread("./test.png")
-esc = "\e{4;36;12{9;image/png{"
-cmd = esc + img.length.to_s(16) + ";" + img + "}"
-if ENV['TERM'] == 'natty'
-    puts cmd
-else
-    puts "#{cmd.length} = #{img.length.to_s} + #{esc.length} + #{img.length.to_s(16).length} + 2"
-end
+img = IO.binread('test.png')
+print "\x1b{4;12;8{9;image/png{" + img.length.to_s(16) + ';' + img + '}'
