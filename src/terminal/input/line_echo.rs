70502f072ed2ff66df7ea9a5a661dd6dc5344766
Without Boats <woboats@gmail.com>
2015-12-13 19:57:02 -0800

    Tests and bug fixes for line buffer.

--- src/terminal/input/line_echo.rs
+++ src/terminal/input/line_echo.rs
@@ -49,19 +49,25 @@ impl LineEcho {
             Char(c) if c == self.settings.lnext as char   => unimplemented!(),
             Char(c) if c == self.settings.werase as char  => unimplemented!(),
             Char(c) if c.width().is_some() => {
+                self.position += 1;
                 self.len += 1;
                 wrap(Put::new_char(c))
             }
             LeftArrow if self.position != 0 => {
                 self.position -= 1;
-                wrap(Move::new(To(Left, 1, false)))
+                wrap(Move::new(To(Left, 1, true)))
             }
             RightArrow if self.position != self.len => {
                 self.position += 1;
-                wrap(Move::new(To(Right, 1, false)))
+                wrap(Move::new(To(Right, 1, true)))
             }
-            Enter       => wrap(Move::new(NextLine(1))),
+            Enter       => {
+                self.position = 0;
+                self.len = 0;
+                wrap(Move::new(NextLine(1)))
+            }
             Backspace if self.position != 0 => {
+                self.position -= 1;
                 self.len -= 1;
                 wrap(CommandSeries(vec![
                     Box::new(Move::new(To(Left, 1, false))) as Box<Command>,
@@ -72,9 +78,14 @@ impl LineEcho {
                 self.len -= 1;
                 wrap(RemoveChars::new(1))
             }
-            Home        => {
+            Home if self.position != 0 => {
                 self.position = 0;
                 wrap(Move::new(To(Left, self.position, true)))
+            }
+            End if self.position != self.len => {
+                let n = self.len - self.position;
+                self.position = self.len;
+                wrap(Move::new(To(Right, n, true)))
             }
             _           => None
         }
@@ -83,4 +94,103 @@ impl LineEcho {
 
 fn wrap<T: Command>(cmd: T) -> Option<Box<Command>> {
     Some(Box::new(cmd) as Box<Command>)
+}
+
+#[cfg(test)]
+mod tests {
+
+    use command::*;
+    use datatypes::EchoSettings;
+    use datatypes::Key::*;
+    use datatypes::Area::*;
+    use datatypes::Movement::*;
+    use datatypes::Direction::*;
+
+    use super::*;
+
+    static SETTINGS: EchoSettings = EchoSettings { lerase: 0, lnext: 1, werase: 2 };
+
+    #[test]
+    fn chars() {
+        let mut echo = LineEcho::new(SETTINGS);
+        assert_eq!(echo.echo(Char('A')).unwrap().repr(), Put::new_char('A').repr());
+        assert_eq!(echo.len, 1);
+        assert_eq!(echo.position, 1);
+        assert!(echo.echo(Char('\x1b')).is_none());
+        assert_eq!(echo.len, 1);
+        assert_eq!(echo.position, 1);
+    }
+
+    #[test]
+    fn left_arrow() {
+        let mut echo = LineEcho { settings: SETTINGS, position: 0, len: 0 };
+        assert!(echo.echo(LeftArrow).is_none());
+        assert_eq!(echo.len, 0);
+        assert_eq!(echo.position, 0);
+        let mut echo = LineEcho { settings: SETTINGS, position: 1, len: 1 };
+        assert_eq!(echo.echo(LeftArrow).unwrap().repr(), Move::new(To(Left, 1, true)).repr());
+        assert_eq!(echo.len, 1);
+        assert_eq!(echo.position, 0);
+    }
+
+    #[test]
+    fn right_arrow() {
+        let mut echo = LineEcho { settings: SETTINGS, position: 0, len: 0 };
+        assert!(echo.echo(RightArrow).is_none());
+        assert_eq!(echo.len, 0);
+        assert_eq!(echo.position, 0);
+        let mut echo = LineEcho { settings: SETTINGS, position: 0, len: 1 };
+        assert_eq!(echo.echo(RightArrow).unwrap().repr(), Move::new(To(Right, 1, true)).repr());
+        assert_eq!(echo.len, 1);
+        assert_eq!(echo.position, 1);
+    }
+
+    #[test]
+    fn enter() {
+        let mut echo = LineEcho { settings: SETTINGS, position: 3, len: 3 };
+        assert_eq!(echo.echo(Enter).unwrap().repr(), Move::new(NextLine(1)).repr());
+        assert_eq!(echo.len, 0);
+        assert_eq!(echo.position, 0);
+    }
+
+    #[test]
+    fn backspace() {
+        let mut echo = LineEcho { settings: SETTINGS, position: 0, len: 1 };
+        assert!(echo.echo(Backspace).is_none());
+        assert_eq!(echo.position, 0);
+        assert_eq!(echo.len, 1);
+        let mut echo = LineEcho { settings: SETTINGS, position: 1, len: 1 };
+        assert!(echo.echo(Backspace).is_some());
+        assert_eq!(echo.position, 0);
+        assert_eq!(echo.len, 0);
+    }
+
+    #[test]
+    fn delete() {
+        let mut echo = LineEcho { settings: SETTINGS, position: 0, len: 1 };
+        assert!(echo.echo(Delete).is_some());
+        assert_eq!(echo.position, 0);
+        assert_eq!(echo.len, 0);
+        let mut echo = LineEcho { settings: SETTINGS, position: 1, len: 1 };
+        assert!(echo.echo(Delete).is_none());
+        assert_eq!(echo.position, 1);
+        assert_eq!(echo.len, 1);
+    }
+
+    #[test]
+    fn home() {
+        let mut echo = LineEcho { settings: SETTINGS, position: 4, len: 5 };
+        assert!(echo.echo(Home).is_some());
+        assert_eq!(echo.position, 0);
+        assert_eq!(echo.len, 5);
+    }
+
+    #[test]
+    fn end() {
+        let mut echo = LineEcho { settings: SETTINGS, position: 4, len: 5 };
+        assert!(echo.echo(End).is_some());
+        assert_eq!(echo.position, 5);
+        assert_eq!(echo.len, 5);
+    }
+
 }
