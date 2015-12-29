f600f4737d1fded310f2b1bfc530f973dfe5c2e9
Without Boats <woboats@gmail.com>
2015-12-28 23:06:04 -0800

    Fix image test.

--- scaffolding/imagetest.rb
+++ scaffolding/imagetest.rb
@@ -1,2 +1,5 @@
-img = IO.binread('test.png')
-puts "\x1b{4;12;8{9;image/png{" + img.length.to_s(16) + ';' + img + '}'
+require 'base64'
+
+img  = Base64.strict_encode64(IO.binread('test.png'))
+mime = Base64.strict_encode64('image/png')
+puts "\x1b_[14;12;8##{mime}##{img}\u{9c}"
