8589bb40433be38361f5b2e960014b8d64cbcc63
Without Boats <woboats@gmail.com>
2015-12-31 20:38:16 -0800

    Make testing script test notty-cairo as well.

--- build_test.sh
+++ build_test.sh
@@ -2,4 +2,8 @@
 # This script tests both the natty and scaffolding crates, to make sure that
 # changes to natty's public API do not break the scaffolding terminal
 
-cargo test && cd scaffolding && cargo test && cd ..
+test_subcrate() {
+    cd $1 && cargo test && cd ..
+}
+
+cargo test && test_subcrate 'notty-cairo' && test_subcrate 'scaffolding'
