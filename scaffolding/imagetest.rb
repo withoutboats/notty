7dd0175520071bb6724f1c3d406d603158cb7b6a
Without Boats <woboats@gmail.com>
2016-02-21 21:47:50 -0800

    Replace the test image with a larger image.
    This will give a better test of performance.

--- scaffolding/imagetest.rb
+++ scaffolding/imagetest.rb
@@ -1,5 +1,5 @@
 require 'base64'
 
-img  = Base64.strict_encode64(IO.binread('test.png'))
-mime = Base64.strict_encode64('image/png')
-puts "\x1b_[14;12;8##{mime}##{img}\u{9c}"
+img  = Base64.strict_encode64(IO.binread('guernica.jpg'))
+mime = Base64.strict_encode64('image/jpeg')
+puts "\x1b_[14;32;8;4##{mime}##{img}\u{9c}"
