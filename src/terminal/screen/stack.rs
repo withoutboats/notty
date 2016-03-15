eb6d5e12e2eb1ee954dbb545f685231dc4133d5b
Without Boats <woboats@gmail.com>
2016-03-14 19:17:57 -0700

    Add tests for stack, make them pass.

--- src/terminal/screen/stack.rs
+++ src/terminal/screen/stack.rs
@@ -1,5 +1,6 @@
 use std::mem;
 
+#[derive(Clone, Debug, Eq, PartialEq)]
 pub struct Stack<T> {
     pub top: T,
     rest: Option<Vec<T>>,
@@ -88,13 +89,13 @@ pub struct Iter<'a, T: 'a> {
 impl<'a, T> Iterator for Iter<'a, T> {
     type Item = &'a T;
     fn next(&mut self) -> Option<&'a T> {
-        self.top.take().or_else(|| self.rest.as_mut().and_then(|iter| iter.next()))
+        self.top.take().or_else(|| self.rest.as_mut().and_then(|iter| iter.next_back()))
     }
 }
 
 impl<'a, T> DoubleEndedIterator for Iter<'a, T> {
     fn next_back(&mut self) -> Option<&'a T> {
-        self.rest.as_mut().and_then(|iter| iter.next_back()).or_else(|| self.top.take())
+        self.rest.as_mut().and_then(|iter| iter.next()).or_else(|| self.top.take())
     }
 }
 
@@ -106,13 +107,13 @@ pub struct IterMut<'a, T: 'a> {
 impl<'a, T> Iterator for IterMut<'a, T> {
     type Item = &'a mut T;
     fn next(&mut self) -> Option<&'a mut T> {
-        self.top.take().or_else(|| self.rest.as_mut().and_then(|iter| iter.next()))
+        self.top.take().or_else(|| self.rest.as_mut().and_then(|iter| iter.next_back()))
     }
 }
 
 impl<'a, T> DoubleEndedIterator for IterMut<'a, T> {
     fn next_back(&mut self) -> Option<&'a mut T> {
-        self.rest.as_mut().and_then(|iter| iter.next_back()).or_else(|| self.top.take())
+        self.rest.as_mut().and_then(|iter| iter.next()).or_else(|| self.top.take())
     }
 }
 
@@ -124,12 +125,108 @@ pub struct IntoIter<T> {
 impl<T> Iterator for IntoIter<T> {
     type Item = T;
     fn next(&mut self) -> Option<T> {
-        self.top.take().or_else(|| self.rest.as_mut().and_then(|iter| iter.next()))
+        self.top.take().or_else(|| self.rest.as_mut().and_then(|iter| iter.next_back()))
     }
 }
 
 impl<T> DoubleEndedIterator for IntoIter<T> {
     fn next_back(&mut self) -> Option<T> {
-        self.rest.as_mut().and_then(|iter| iter.next_back()).or_else(|| self.top.take())
+        self.rest.as_mut().and_then(|iter| iter.next()).or_else(|| self.top.take())
     }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    use std::fmt::Debug;
+
+    fn one_stack() -> Stack<i32> {
+        Stack {
+            top: 0,
+            rest: None,
+        }
+    }
+
+    fn three_stack() -> Stack<i32> {
+        Stack {
+            top: 2,
+            rest: Some(vec![0, 1]),
+        }
+    }
+
+    fn run_test<F, T>(f: F, res: [T; 2]) where F: Fn(Stack<i32>) -> T, T: PartialEq + Debug {
+        assert_eq!(f(one_stack()), res[0]);
+        assert_eq!(f(three_stack()), res[1]);
+    }
+
+    #[test]
+    fn new() {
+        assert_eq!(Stack::new(0), one_stack())
+    }
+
+    #[test]
+    fn push() {
+        run_test(|mut stack| { stack.push(5); stack }, [
+            Stack { top: 5, rest: Some(vec![0]) },
+            Stack { top: 5, rest: Some(vec![0, 1, 2]) },
+        ])
+    }
+
+    #[test]
+    fn pop() {
+        run_test(|mut stack| { stack.pop(); stack }, [
+            Stack { top: 0, rest: None },
+            Stack { top: 1, rest: Some(vec![0]) },
+        ]);
+    }
+
+    #[test]
+    fn iter() {
+        run_test(|stack| stack.iter().cloned().collect::<Vec<i32>>(), [
+            vec![0],
+            vec![2, 1, 0],
+        ])
+    }
+
+    #[test]
+    fn iter_rev() {
+        run_test(|stack| stack.iter().rev().cloned().collect::<Vec<i32>>(), [
+            vec![0],
+            vec![0, 1, 2],
+        ])
+    }
+
+    #[test]
+    fn iter_mut() {
+        run_test(|mut stack| stack.iter_mut().map(|&mut x| x).collect::<Vec<i32>>(), [
+            vec![0],
+            vec![2, 1, 0],
+        ])
+    }
+
+    #[test]
+    fn iter_mut_rev() {
+        run_test(|mut stack| stack.iter_mut().rev().map(|&mut x| x).collect::<Vec<i32>>(), [
+            vec![0],
+            vec![0, 1, 2],
+        ])
+    }
+
+    #[test]
+    fn into_iter() {
+        run_test(|stack| stack.into_iter().collect::<Vec<i32>>(), [
+            vec![0],
+            vec![2, 1, 0],
+        ])
+    }
+
+    #[test]
+    fn into_iter_rev() {
+        run_test(|stack| stack.into_iter().rev().collect::<Vec<i32>>(), [
+            vec![0],
+            vec![0, 1, 2],
+        ])
+    }
+
 }
