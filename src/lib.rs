5aa664448845e8bcb40954d034b661dac852ff35
SolarAquarion <shlomochoina@gmail.com>
2016-07-04 20:14:56 -0400

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,7 @@
 //  
 //  You should have received a copy of the GNU Affero General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#![feature(pub_restricted)]
 #![feature(io, iter_arith)]
 
 extern crate base64;
