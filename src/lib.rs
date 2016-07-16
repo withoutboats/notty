072100449961d116f27fa44c2350df6aaeeb543f
Michael Mior <michael.mior@gmail.com>
2016-07-16 10:51:32 -0400

    Fix build warnings

--- src/lib.rs
+++ src/lib.rs
@@ -14,7 +14,7 @@
 //  You should have received a copy of the GNU Affero General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #![feature(pub_restricted)]
-#![feature(io, iter_arith)]
+#![feature(io)]
 
 extern crate base64;
 extern crate mime;
--- src/terminal/mod.rs
+++ src/terminal/mod.rs
@@ -13,7 +13,7 @@
 //  
 //  You should have received a copy of the GNU Affero General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-use std::io::{self, Write};
+use std::io::{self};
 use std::ops::{Deref, DerefMut};
 use std::sync::atomic::Ordering::Relaxed;
 
